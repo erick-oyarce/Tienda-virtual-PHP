0a86839f52b394532f99b6482e0f27091ee4067f
erick-oyarce <42985233+erick-oyarce@users.noreply.github.com>
2019-03-21 19:56:57 -0300

    Actualizacion en la hoja de estilo y HTML
    Se eliminarion estilo obtenidos de otras fuentes y se adaptaron al contenido de la pagina. Los estilos añadidos no generan una aparecia extremadamente optima, pues la finalidad del ejemplo es la funcionalidad mas que la vista de la pagina

--- index.php
+++ index.php
@@ -3,102 +3,115 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <title>Tienda Virtual</title>
-        
+
         <!--Hoja de estilos-->
         <link rel="stylesheet" href="assets/css/styles.css">
-        
+
     </head>
     <body>
-        <!--Cabecera-->
-        <header id="header">
-            <div id="logo">
-                <img src="assets/img/camiseta.png" alt="Camiseta Logo">
-                <a href="index.php">
-                    Tienda de Camisetas                    
-                </a>
-            </div>
-        </header>
-        
-        <!--Menú-->
-        <nav id="menu">
-            <ul>
-                <li>
-                    <a href="#">Inicio</a>
-                </li>
-                <li>
-                    <a href="#">Categoria 1</a>
-                </li>
-                <li>
-                    <a href="#">Categoria 2</a>
-                </li>
-                <li>
-                    <a href="#">Categoria 3</a>
-                </li>
-                <li>
-                    <a href="#">Categoria 4</a>
-                </li>
-            </ul>
-            
-        </nav>
-        
-        <div id="content">
-        <!--Barra Lateral-->
-        <aside id="laterla">
-            
-            <div id="login" class="block_aside">
-                
-                <form action="" method="POST">
-                    <label for="email">Email</label>
-                    <input type="email" name="email">
+        <div id="container">
+            <!--Cabecera-->
+            <header id="header">
+                <div id="logo">
+                    <img src="assets/img/camiseta.png" alt="Camiseta Logo">
+                    <a href="index.php">
+                        Tienda de Camisetas                    
+                    </a>
+                </div>
+            </header>
+
+            <!--Menú-->
+            <nav id="menu">
+                <ul>
+                    <li>
+                        <a href="#">Inicio</a>
+                    </li>
+                    <li>
+                        <a href="#">Categoria 1</a>
+                    </li>
+                    <li>
+                        <a href="#">Categoria 2</a>
+                    </li>
+                    <li>
+                        <a href="#">Categoria 3</a>
+                    </li>
+                    <li>
+                        <a href="#">Categoria 4</a>
+                    </li>
+                </ul>
+
+            </nav>
+
+            <div id="content">
+                <!--Barra Lateral-->
+                <aside id="lateral">
+
+                    <div id="login" class="block_aside">
+                        <h3>Entrar a la Web</h3>
+
+                        <form action="" method="POST">
+                            <label for="email">Email</label>
+                            <input type="email" name="email">
+
+                            <label for="password">Contraseña</label>
+                            <input type="password" name="password">
+
+                            <input type="submit" value="Enviar">
+
+                        </form>
+                        
+                        <ul>
+                            <li>
+                                <a href="#">Mis pedidos</a>
+                            </li>
+                            <li>
+                                <a href="#">Gestionar Pedidos</a>
+                            </li>
+                            <li>
+                                <a href="#">Gestionar Categorias</a>
+                            </li>
+                            
+                        </ul>
+
+                    </div>
+
+                </aside>
+
+                <!--Contenido central-->
+                <div id="central">
                     
-                    <label for="password">Contraseña</label>
-                    <input type="password" name="password">
-                    
-                    <input type="submit" value="Enviar">
-                    
-                </form>
-                
-                <a href="#">Mis pedidos</a>
-                <a href="#">Gestionar Pedidos</a>
-                <a href="#">Gestionar Categorias</a>
-                
-                
+                    <h1>Productos destacados</h1>
+
+                    <div class="product">
+                        <img src="assets/img/camiseta.png">
+                        <h2>Camiseta Azul</h2>
+                        <p>30.000 CLP</p>
+                        <a href="" class="button">Comprar</a>
+                    </div>
+                    <div class="product">
+                        <img src="assets/img/camiseta.png">
+                        <h2>Camiseta Azul</h2>
+                        <p>30.000 CLP</p>
+                        <a href="" class="button">Comprar</a>
+                    </div>
+                    <div class="product">
+                        <img src="assets/img/camiseta.png">
+                        <h2>Camiseta Azul</h2>
+                        <p>30.000 CLP</p>
+                        <a href="" class="button">Comprar</a>
+                    </div>
+
+                </div>
+
+
             </div>
-            
-        </aside>
-        
-        <!--Contenido central-->
-        <div id="central">
-            
-            <div class="product">
-                <img src="assets/img/camiseta.png">
-                <h2>Camiseta Azul</h2>
-                <p>30.000 CLP</p>
-                <a href="">Comprar</a>
-            </div>
-            <div class="product">
-                <img src="assets/img/camiseta.png">
-                <h2>Camiseta Azul</h2>
-                <p>30.000 CLP</p>
-                <a href="">Comprar</a>
-            </div>
-            <div class="product">
-                <img src="assets/img/camiseta.png">
-                <h2>Camiseta Azul</h2>
-                <p>30.000 CLP</p>
-                <a href="">Comprar</a>
-            </div>
-            
+
+            <!--Pie de pagina-->
+            <footer id="footer">
+                <p>Desarrollado por Erick Oyarce &copy; <?= date('y') ?></p>
+
+            </footer>
         </div>
-        
-        
-        </div>
-        
-        <!--Pie de pagina-->
-        <footer id="footer">
-            <p>Desarrollado por Erick Oyarce &copy; <?=date('y')?></p>
-            
-        </footer>
-        
+
     </body>
 </html>
